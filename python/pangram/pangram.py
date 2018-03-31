4c538f2c62b2b374cc918dcb79e0b345f0c9ae67
Adrianacmy <adrianamullaney@gmail.com>
2018-03-31 15:23:33 -0700

    add solution to pangram

---
+++ python/pangram/pangram.py
@@ -0,0 +1,10 @@
+import string
+
+def is_pangram(sentence):
+    letters = string.ascii_lowercase
+    sentence = sentence.lower()
+    for letter in letters:
+        if letter not in sentence:
+            return False
+    return True
+
